a269e8270d1cfd47695e8166776c3527e853c43f
Alexandre Nazareth <alexandrenazareth@hotmail.com>
2025-03-30 14:16:37 -0300

    feat(security): ApplicationConfig updated

--- src/main/java/com/projetointegrador/seumentor/security/config/ApplicationConfig.java
+++ src/main/java/com/projetointegrador/seumentor/security/config/ApplicationConfig.java
@@ -1,9 +1,14 @@
 package com.projetointegrador.seumentor.security.config;
 
+import org.springframework.boot.autoconfigure.couchbase.CouchbaseProperties.Authentication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.projetointegrador.seumentor.user.api.UsersDetailsAdapter;
 
@@ -19,5 +24,17 @@ public class ApplicationConfig {
   public UserDetailsService userDetailsService() {
     return username -> userQuery.findByEmail(username)
         .orElseThrow(() -> new UsernameNotFoundException("user not found"));
+  }
+
+  @Bean
+  public AuthenticationProvider authenticationProvider() {
+    DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+    authProvider.setUserDetailsService(userDetailsService());
+    authProvider.setPasswordEncoder(passwordEncoder());
+    return authProvider;
+  }
+
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
   }
 }
