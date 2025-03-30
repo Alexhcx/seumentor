55ebaec264f0a31afa3378465adec0db0b44efd1
Alexandre Nazareth <alexandrenazareth@hotmail.com>
2025-03-30 14:21:22 -0300

    feat(security): Application Configuration complete

--- src/main/java/com/projetointegrador/seumentor/security/config/ApplicationConfig.java
+++ src/main/java/com/projetointegrador/seumentor/security/config/ApplicationConfig.java
@@ -1,10 +1,11 @@
 package com.projetointegrador.seumentor.security.config;
 
-import org.springframework.boot.autoconfigure.couchbase.CouchbaseProperties.Authentication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -34,6 +35,12 @@ public class ApplicationConfig {
     return authProvider;
   }
 
+  @Bean
+  public AuthenticationManager AuthenticationManager(AuthenticationConfiguration config) throws Exception {
+    return config.getAuthenticationManager();
+  }
+
+  @Bean
   public PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
   }
