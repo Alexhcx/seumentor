51672de58877d47668f43f7b09579ae4fb597f1a
Alexandre Nazareth <alexandrenazareth@hotmail.com>
2025-04-14 09:00:17 -0300

    feat: Remove DisciplinePreference and UserRating models and their repositories

--- src/main/java/com/projetointegrador/seumentor/course/repository/DisciplinePreferenceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.projetointegrador.seumentor.course.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.projetointegrador.seumentor.course.model.DisciplinePreference;
-
-public interface DisciplinePreferenceRepository extends JpaRepository<DisciplinePreference, Integer> {
-
-}
--- src/main/java/com/projetointegrador/seumentor/tutoring/repository/UserRatingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.projetointegrador.seumentor.tutoring.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.projetointegrador.seumentor.tutoring.model.UserRating;
-
-public interface UserRatingRepository extends JpaRepository<UserRating, Integer> {
-
-}
