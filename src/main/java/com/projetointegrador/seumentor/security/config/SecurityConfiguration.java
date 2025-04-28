cbb295cdb45366a2ff9c28375d6f2d83ea9ebb66
Alexandre Nazareth <alexandrenazareth@hotmail.com>
2025-04-28 17:54:46 -0300

    feat: Update CORS configuration to allow access from localhost:3000

--- src/main/java/com/projetointegrador/seumentor/security/config/SecurityConfiguration.java
+++ src/main/java/com/projetointegrador/seumentor/security/config/SecurityConfiguration.java
@@ -3,21 +3,21 @@ package com.projetointegrador.seumentor.security.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.config.Customizer; // Importar Customizer
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration; // Importar CorsConfiguration
-import org.springframework.web.cors.CorsConfigurationSource; // Importar CorsConfigurationSource
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource; // Importar UrlBasedCorsConfigurationSource
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import lombok.RequiredArgsConstructor;
 
-import java.util.Arrays; // Importar Arrays
-import java.util.List; // Importar List
+import java.util.Arrays;
+
 
 @Configuration
 @EnableWebSecurity
@@ -57,7 +57,7 @@ public class SecurityConfiguration {
         @Bean
         CorsConfigurationSource corsConfigurationSource() {
                 CorsConfiguration configuration = new CorsConfiguration();
-                configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:8081", "URL_DO_SEU_FRONTEND_EM_PRODUCAO")); // Adicione a URL do seu frontend Vue.js (desenv e prod)
+                configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:8081", "http://localhost:3000"));
                 configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
                 configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
                 configuration.setAllowCredentials(true);
